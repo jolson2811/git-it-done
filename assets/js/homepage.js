6009a2c23e3ff2326344e7a8b47514cb6db26ebc
jolson2811 <jolson2811@gmail.com>
2022-03-14 21:22:09 -0600

    updated functions to notify users of errors and no data return

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -9,9 +9,17 @@ var getUserRepos = function (user) {
 
   // make a request to the url
   fetch(apiUrl).then(function (response) {
-    response.json().then(function (data) {
-      displayRepos(data, user);
-    });
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
   });
 };
 
@@ -30,6 +38,11 @@ var formSubmitHandler = function (event) {
 };
 
 var displayRepos = function (repos, searchTerm) {
+  // check if api returned any repos
+  if (repos.length === 0) {
+    repoContainerEl.textContent = "No repositories found.";
+    return;
+  }
   // clear old content
   repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
